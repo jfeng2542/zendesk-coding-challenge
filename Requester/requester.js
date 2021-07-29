f5c303e46366dd6e011c919dddbed0879467e14b
Jeremy Feng <jeremyfeng2254@gmail.com>
2021-07-29 16:01:36 -0400

    Partially implemented all tickets request option

--- Requester/requester.js
+++ Requester/requester.js
@@ -15,6 +15,55 @@ class requester {
         .catch(err => console.error(err));
     }
 
+    async requestAllTickets(readlineSync) {
+        let url = 'https://zccjef223.zendesk.com/api/v2/tickets.json?page[size]=25';
+        let prevLink, nextLink;
+        //do {
+            await fetch(url, {
+                headers: { 'Authorization': 'Bearer ' + this.TOKEN }
+            })
+            .then(this.errorCheck)
+            .then(response => response.json())
+            .then(async(data) => {
+                prevLink = data.links.prev;
+                nextLink = data.links.next;
+                await this.linkCheck(prevLink, nextLink);
+            })
+            .catch(err => console.error(err));
+        //} while(check for null);
+    }
+
+    // Checks for valid pagination
+    async linkCheck(prevLink, nextLink) {
+        let urls = [prevLink, nextLink];
+        await Promise.all(urls.map(url => fetch(url, {
+            headers: { 'Authorization': 'Bearer ' + this.TOKEN }
+        })))
+        // .then(map to this.errorCheck)
+        .then(async(responses) => await Promise.all(
+            responses.map(response => response.json())
+        ))
+        .then(jsons => {
+            paginationMsg(jsons[0].links.prev, jsons[1].links.next);
+        })
+        .catch(err => console.error(err));
+    }
+
+    paginationMsg(prevPage, nextPage) {
+        let prevPageExists = true, nextPageExists = true;
+        if(prevPage == null) {
+            prevPageExists = false;
+            console.log('Type \'next\' to view the next page');
+        } else if(nextPage == null) {
+            nextPageExists = false;
+            console.log('Type \'prev\' to view the previous page');
+        } else {
+            console.log('Type \'prev\' to view the previous page');
+            console.log('Type \'next\' to view the next page');
+        }
+        //FIXME: Implement prev/next input system
+    }
+
     errorCheck(response) {
         if(!response.ok) console.log('Error...');
         else return response;
