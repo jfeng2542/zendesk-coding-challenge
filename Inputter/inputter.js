e509b1d911345ad194c4050a41bc5888883998d0
Jeremy Feng <jeremyfeng2254@gmail.com>
2021-07-30 11:51:04 -0400

    Tidied up some code :)

--- Inputter/inputter.js
+++ Inputter/inputter.js
@@ -1,21 +1,32 @@
 const readlineSync = require('readline-sync');
 
 class inputter {
+    /*
+     *  This class responds to user inputs
+     *  @param requester class to be used to respond to user inputs
+     */
     constructor(TicketRequest) {
         this.ticketRequest = TicketRequest;
         this.pages = new Array(3);  // Will be an array that includes the previous, current, and next page
     }
 
+    /*
+     *  Uses readlineSync to prompt for user input
+     *  @param question to ask the user
+     *  @return user's answer to the question
+     */
     askQuestion(strQuestion) {
         return readlineSync.question(strQuestion);
     }
 
-    /*getIDInput() {
-        return readlineSync.question('Type in an ID: ');
-    }*/
-
+    /*
+     *  Responds to initial menu options
+     *  @param user's inputted choice from the menu
+     *  @return true/false if the user typed 'exit'
+     */
     async inputOptions(answer) {
         let inputIsExit = false, inputIsStop = false;
+
         switch(answer) {
             case 'menu':
                 console.log('Type \'1\' to view all tickets');
@@ -23,12 +34,11 @@ class inputter {
                 console.log('Type \'exit\' to quit');
                 break;
             case '1':
-                let url = 'https://zccjef223.zendesk.com/api/v2/tickets.json?page[size]=25';
-                this.pages = await this.ticketRequest.requestAllTickets(url);
+                this.pages = await this.ticketRequest.requestAllTickets();
                 do {
-                    console.log('(hehehe prepare to face my pun-believable puns!!!)');
-                    answer = this.askQuestion('Check out the \'prev\' page, \'next\' page, or \'stop\' looking at all the tickets: ');
-                    inputIsStop = await this.paginationInputOptions(answer);
+                console.log('(hehehe prepare to face my pun-believable puns!!!)');
+                answer = this.askQuestion('Check out the \'prev\' page, \'next\' page, or \'stop\' looking at all the tickets: ');
+                inputIsStop = await this.paginationInputOptions(answer);
                 } while(inputIsStop == false);
                 break;
             case '2':
@@ -37,37 +47,39 @@ class inputter {
             case 'exit':
                 inputIsExit = true;
                 console.log('Bye, have a great day!');
-                break;
+                break;             
             default:
                 console.log('Sorry, my vocabulary doesn\'t go that far. I skipped high school :/');
         }
         return inputIsExit;
     }
 
-    // Before this, it will loop and ask 'Input \'prev\' to see the previous page or \'next\' for the next page'
+    /*
+     *  Responds to all-tickets menu options
+     *  @param user's inputted choice from the menu
+     *  @return true/false if the user typed 'stop'
+     */
     async paginationInputOptions(answer) {
         let inputIsStop = false;
+
         switch(answer) {
             case 'prev':
                 if(this.pages[0].tickets.length == 0) console.log("Sorry, this page doesn't exist :/");
-                else {  // Prints previous page, then sends previous page's url to fetch again
-                    //console.log(this.pages[0]);
-                    this.pages = await this.ticketRequest.requestAllTickets(this.pages[1].links.prev);
-                }
+                // Else, send previous page's url to fetch again
+                else this.pages = await this.ticketRequest.requestAllTickets(this.pages[1].links.prev);
                 break;
             case 'next':
                 if(this.pages[2].tickets.length == 0) console.log("Sorry, this page doesn't exist :/");
-                else {  // Prints next page, then sends next page's url to fetch again
-                    //console.log(this.pages[2]);
-                    this.pages = await this.ticketRequest.requestAllTickets(this.pages[1].links.next);
-                }
+                // Else, send next page's url to fetch again
+                else this.pages = await this.ticketRequest.requestAllTickets(this.pages[1].links.next);
                 break;
             case 'stop':
-                inputIsStop = true;
                 console.log('Now leaving the all-tickets menu...');
+                inputIsStop = true;
                 break;
             default:
-                console.log('Sorry, my vocabulary doesn\'t go that far. I skipped high school :/');
+                console.log('Sorry, I do\'t know what you just said. I also forgot words I used to know some time ago...');
+                console.log('...I need to go back to school...');
         }
         return inputIsStop;
     }
--- Requester/requester.js
+++ Requester/requester.js
@@ -1,85 +1,97 @@
 const fetch = require('node-fetch');
+const fetchHeaders = require('fetch-headers');
 
 class requester {
+    /*
+     *  This class handles fetch requests and errors
+     *  @param authorization token
+     */
     constructor(token) {
         this.TOKEN = token;
+        this.authHeader = new fetchHeaders();
+        this.authHeader.append('Authorization', 'Bearer ' + this.TOKEN);
     }
 
+    /*
+     *  Gets a ticket with a specific ID
+     *  @param user-requested ticket ID
+     */
     async requestOneTicket(ticket_id) {
-        await fetch('https://zccjef223.zendesk.com/api/v2/tickets/' + ticket_id + '.json', {
-            headers: { 'Authorization': 'Bearer ' + this.TOKEN }
-        })
-        .then(this.errorCheck)  // References errorCheck with a fetch response instead of calling the function
+        let urlFirstPart = 'https://zccjef223.zendesk.com/api/v2/tickets/';
+
+        await fetch(urlFirstPart + ticket_id + '.json', { headers: this.authHeader })
+        .then(this.errorCheck)  // References errorCheck with the fetch response instead of calling the function
         .then(response => response.json())
-        .then(data => console.log('Ticket with subject \'' + data.ticket.subject + '\''))
-        .catch(err => console.error(err));
+        .then(data => console.log('Ticket with subject \'' + data.ticket.subject + '\''))//FIXME: Need better format
+        .catch(err => { throw new Error('Oh no, something bad happened! It\'s not your fault though...I think.') });
     }
 
-    async requestAllTickets(url) {
-        //let url = 'https://zccjef223.zendesk.com/api/v2/tickets.json?page[size]=25';
+    /*
+     *  Gets all the tickets and pages through 25 tickets
+     *  @param url that returns the first 25 tickets
+     *  @return array that includes the previous, current, and next page, respectively
+     */
+    async requestAllTickets() {
+        let url = 'https://zccjef223.zendesk.com/api/v2/tickets.json?page[size]=25';
         let prevLink, nextLink;
-        let pages;  // Will be an array that includes the previous, current, and next page
+        let pages;
 
-        await fetch(url, {
-            headers: { 'Authorization': 'Bearer ' + this.TOKEN }
-        })
+        await fetch(url, { headers: this.authHeader })
         .then(this.errorCheck)
         .then(response => response.json())
         .then(async(data) => {
             prevLink = data.links.prev;
             nextLink = data.links.next;
             pages = await this.linkCheck(prevLink, nextLink);
-            pages[1] = data;    // data is the current page json
+            pages[1] = data;    // After 'pages' is initialized with prev & next pages, the 2nd index will hold current page data
             return console.log(data.tickets.map(ticket => console.log(ticket.id)));
         })
-        .catch(err => console.error(err));
+        .catch(err => { throw new Error('Oh no, something bad happened! It\'s not your fault though...I think.') });
 
         return pages;
     }
 
-    // Loads the previous and next page to be checked if they exist and if they can be used in the menu
+    /*
+     *  Loads the previous and next page to be checked if they exist and if they can be used in the menu
+     *  @param links to the previous and next page
+     *  @return array that includes the previous page and next page data
+     */
     async linkCheck(prevLink, nextLink) {
         let urls = [prevLink, nextLink];
         let jsonPages = new Array(3);
-        await Promise.all(urls.map(url => fetch(url, {
-            headers: { 'Authorization': 'Bearer ' + this.TOKEN }
-        })))
-        // .then(map to this.errorCheck)
+
+        await Promise.all(urls.map(
+            url => fetch(url, { headers: this.authHeader })
+        .then(this.errorCheck)))
         .then(async(responses) => await Promise.all(
-            responses.map(response => response.json())
-        ))
+            responses.map(response => response.json()
+        )))
         .then(jsons => {
-            jsonPages[0] = jsons[0];
-            jsonPages[2] = jsons[1];
-            //paginationMsg(jsons[0].links.prev, jsons[1].links.next);
+            jsonPages[0] = jsons[0];    // Stores previous page data into 1st index
+            jsonPages[2] = jsons[1];    // Stores next page data into 3rd index
         })
-        .catch(err => console.error(err));
+        .catch(err => { throw new Error('Oh no, something bad happened! It\'s not your fault though...I think.') });
 
         return jsonPages;
     }
 
-    /*paginationMsg(prevPage, nextPage) {
-        let prevPageExists = true, nextPageExists = true;
-        if(prevPage == null) {
-            prevPageExists = false;
-            console.log('Type \'next\' to view the next page');
-        } else if(nextPage == null) {
-            nextPageExists = false;
-            console.log('Type \'prev\' to view the previous page');
-        } else {
-            console.log('Type \'prev\' to view the previous page');
-            console.log('Type \'next\' to view the next page');
-        }
-        //FIXME: Implement prev/next input system
-    }*/
-
+    /*
+     *  Checks if the fetch response is not an error
+     *  @param fetch response
+     *  @return fetch response
+     */
     errorCheck(response) {
-        if(!response.ok) console.log('Error...');
+        if(!response.ok) {
+            switch(response.status) {
+                case '400':
+                    throw new Error("Invalid ticket ID");
+                case '401':
+                    throw new Error("Authentication error");
+                case '404':
+                    throw new Error("Ticket ID doesn't exist");
+            }
+        }
         else return response;
-        /*
-        401 - Authentication error
-        404 - Ticket ID doesn't exist
-        */
     }
 }
 
